36fc3807b7c17bd9bf232e6d85ffd8c34416eb91
Rizzu1023 <mrizwan9022@gmail.com>
2019-02-26 21:08:38 +0530

    initial commit

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -30,21 +30,6 @@ posts = [
 def home():
     return render_template('home.html', posts= posts)
 
-@app.route('/login', methods=['GET','POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else  redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. please check username & password', 'danger')
-    return render_template('login.html', form=form)
-
 @app.route("/register", methods=['GET','POST'])
 def register():
     if current_user.is_authenticated:
