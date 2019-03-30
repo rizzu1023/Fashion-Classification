47d9f9e516880fe33b752a5b428abb2342f4d52d
Rizzu1023 <mrizwan9022@gmail.com>
2019-03-30 23:01:01 +0530

    registration part done

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -24,6 +24,25 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That email is taken. Please choose a different email')
 
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',validators=[DataRequired(), Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('confirm_Password',validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign Up')
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different username')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different email')
+
 
 
 
